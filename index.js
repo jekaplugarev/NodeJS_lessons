34418040533624474ac3fe6f957f44fcb8de1f6f
jekaplugarev <jeka_plugarev@mail.ru>
2022-01-13 16:33:50 +0800

    finish course

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const PORT = process.env.PORT || 5000
 const Application = require('./framework/Application')
 const userRouter = require('./src/user-router')
+const jsonParser = require('./framework/parseJson')
+const urlParser = require('./framework/parseUrl')
+const mongoose = require('mongoose')
 
+const PORT = process.env.PORT || 5000
 const app = new Application()
 
+app.use(jsonParser)
+app.use(urlParser('http://localhost:5000'))
+
 app.addRouter(userRouter)
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://JekaPl:JekaPl1968@cluster0.i1sxz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
--- src/user-router.js
+++ src/user-router.js
@@ -1,17 +1,10 @@
 const Router = require('../framework/Router')
+const controller = require('./user-controller')
 
 const router = new Router()
 
-const users = [
-    {id: 1, name: 'Jeka'},
-    {id: 2, name: 'Kate'},
-]
+router.get('/users', controller.getUsers)
 
-router.get('/users', (req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'application/json'
-    })
-    res.end(JSON.stringify(users))
-})
+router.post('/users', controller.createUsers)
 
 module.exports = router
